dc77e3b7923e237c49d335d4037ed905564daea5
beanc16 <bean.christopher@outlook.com>
2023-07-08 22:28:19 -0400

    Save progress

--- Assets/_/Scripts/Drag & Drop/IngredientDraggable.cs
+++ Assets/_/Scripts/Drag & Drop/IngredientDraggable.cs
@@ -22,17 +22,32 @@ public class IngredientDraggable : Draggable
 
 
 
+    private void Start()
+    {
+        this.OnEnable();
+    }
+
     private void OnEnable()
     {
         this.toolDropTargetBackgroundsManager = FindObjectOfType<ToolDropTargetBackgroundsManager>();
         this.ingredientPanel = this.GetComponent<IngredientPanel>();
         this.RefreshSprite();
-        OnDragStart.AddListener(() => this.toolDropTargetBackgroundsManager.ToggleAllBackgroundVisibilities(true));
-        OnDragEnd.AddListener(() => this.toolDropTargetBackgroundsManager.ToggleAllBackgroundVisibilities(false));
+        OnDragStart.AddListener(() => this.ToggleAllBackgroundVisibilities(true));
+        OnDragEnd.AddListener(() => this.ToggleAllBackgroundVisibilities(false));
     }
 
     private void RefreshSprite()
     {
         this.ingredientPanel.RefreshSprite();
+    }
+
+    private void ToggleAllBackgroundVisibilities(bool shouldBeVisible)
+    {
+        if (this.toolDropTargetBackgroundsManager == null)
+        {
+            this.toolDropTargetBackgroundsManager = FindObjectOfType<ToolDropTargetBackgroundsManager>();
+        }
+
+        this.toolDropTargetBackgroundsManager.ToggleAllBackgroundVisibilities(shouldBeVisible);
     }
 }
